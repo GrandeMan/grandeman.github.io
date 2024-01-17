e89a91bf9a763db2e392c6fcf1e74a27e90394f7
Joshua Morales <114616062+GrandeMan@users.noreply.github.com>
2024-01-17 07:17:36 +0000

    Update navigation styling in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,10 +22,10 @@ export default function Home() {
 		>
 			<header className="absolute inset-x-0 top-0 z-50">
 				<nav
-					className="flex items-center justify-between p-6 lg:px-8"
+					className="flex items-center justify-between p-6"
 					aria-label="Global"
 				>
-					<div className="flex lg:flex-1">
+					<div>
 						<a href="#" className="-m-1.5 p-1.5">
 							<span className="sr-only">Joshua Morales</span>
 							<img
@@ -37,23 +37,27 @@ export default function Home() {
 							/>
 						</a>
 					</div>
-					<div className="flex lg:hidden">
-						{/* <button
+					{/* <div className="flex lg:hidden">
+						<button
 							type="button"
 							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
 							onClick={() => setMobileMenuOpen(true)}
 						>
 							<span className="sr-only">Open main menu</span>
 							<Bars3Icon className="h-6 w-6" aria-hidden="true" />
-						</button> */}
-					</div>
+						</button>
+					</div> */}
 					{/* <div className="hidden lg:flex lg:gap-x-12"> */}
-					<div className="flex space-x-12">
+					<div className="flex gap-4 ">
 						{navigation.map((item) => (
 							<a
 								key={item.name}
 								href={item.href}
-								className="text-sm font-semibold leading-6 text-gray-900"
+								className={`text-md font-bold leading-6 ${
+									darkMode
+										? "text-orange-50"
+										: "text-gray-900"
+								} hover:text-orange-600 transition-colors duration-300`}
 							>
 								{item.name}
 							</a>
